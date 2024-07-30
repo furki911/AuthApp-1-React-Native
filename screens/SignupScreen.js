5872a737bd2bef6825547d794b46ea919a99aa13
Furqan Masood <bhatfurqan@gmail.com>
2024-07-30 23:16:28 +0530

    Auth Error Handling

--- screens/SignupScreen.js
+++ screens/SignupScreen.js
@@ -2,13 +2,21 @@ import { useState } from "react";
 import { createUser } from "../util/auth";
 import AuthContent from "../components/Auth/AuthContent";
 import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { Alert } from "react-native";
 
 function SignupScreen() {
   const [isAuthenticating, setIsAuthenticating] = useState(false);
 
   const signupHandler = async ({ email, password }) => {
     setIsAuthenticating(true);
-    await createUser(email, password);
+    try {
+      await createUser(email, password);
+    } catch {
+      Alert.alert(
+        "Authentication failed!",
+        "Could not create user. Please check your input and try again later."
+      );
+    }
     setIsAuthenticating(false);
   };
 
