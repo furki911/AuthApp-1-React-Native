ca67326e5964e14105fc83e7083a71484bec0475
Furqan Masood <bhatfurqan@gmail.com>
2024-07-30 23:01:01 +0530

    Logging In

--- util/auth.js
+++ util/auth.js
@@ -1,13 +1,20 @@
 import axios from "axios";
 const API_KEY = "AIzaSyCHTMh4wyRed_6EW8hAYfSWxYW5oa8nWcI";
-const AUTH_URL = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
 
-export const createUser = async (email, password) => {
-  const response = await axios.post(`${AUTH_URL}`, {
+const authenticate = async (mode, email, password) => {
+  const url = `https://identitytoolkit.googleapis.com/v1/accounts:${mode}?key=${API_KEY}`;
+  const response = await axios.post(url, {
     email,
     password,
     returnSecureToken: true,
   });
 
-  console.log(response, "RES");
+  console.log(response.data, "DATA");
+};
+
+export const createUser = async (email, password) => {
+  await authenticate("signUp", email, password);
+};
+export const login = async (email, password) => {
+  await authenticate("signInWithPassword", email, password);
 };
